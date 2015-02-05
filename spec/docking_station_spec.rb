8f4c4ec669fce92b0beb60c3939be283732350d4
Rizwan Ali <rizcube@gmail.com>
2015-02-05 09:46:54 +0000

    included container and added bike container spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -38,4 +38,13 @@ describe DockingStation do
     expect{ station.dock(bike) }.to raise_error(RuntimeError, 'Station is full')
   end
 
+
+  it 'should proivde the list of available bikes' do 
+    working_bike, broken_bike = Bike.new, Bike.new
+    broken_bike.break!
+    station.dock(working_bike)
+    station.dock(broken_bike)
+    expect(station.available_bikes).to eq([working_bike])
+  end
+
 end
